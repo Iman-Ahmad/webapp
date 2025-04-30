f6616df234fa4ee608f6e8d03f3742fcd5c1b22e
Iman-Ahmad <iman.ahmadncd@gmail.com>
2025-04-30 13:19:56 +0300

    fix: security updates and mitosis config

--- apps/components/mitosis.config.js
+++ apps/components/mitosis.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   files: 'src/**/*.lite.tsx',
   targets: [
-    { name: 'react', output: './output/react' },  // Explicit mapping
-    { name: 'vue', output: './output/vue' },
-    { name: 'solid', output: './output/solid' },
-    { name: 'svelte', output: './output/svelte' }
+    { name: 'react', output: '/output/react' },  // Explicit mapping
+    { name: 'vue', output: '/output/vue' },
+    { name: 'solid', output: '/output/solid' },
+    { name: 'svelte', output: '/output/svelte' }
   ]
 };
