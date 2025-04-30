9d0362496fc59700e0a65f0c5914b936bbd8f9f0
Iman-Ahmad <iman.ahmadncd@gmail.com>
2025-04-30 12:34:07 +0300

    fix: security updates and mitosis config

--- apps/components/mitosis.config.js
+++ apps/components/mitosis.config.js
@@ -1,24 +1,15 @@
-// mitosis.config.js
-const { defineConfig } = require('@builder.io/mitosis/config');
-
-module.exports = defineConfig({
+module.exports = {
   files: 'src/**/*.lite.tsx',
   targets: [
-    { 
-      name: 'react',
-      output: './output/react' 
-    },
-    { 
-      name: 'vue',
-      output: './output/vue' 
-    },
-    { 
-      name: 'solid',
-      output: './output/solid' 
-    },
-    { 
-      name: 'svelte',
-      output: './output/svelte' 
-    }
-  ]
-});
+    'react',
+    'vue',
+    'solid',
+    'svelte'
+  ],
+  output: {
+    react: './output/react',
+    vue: './output/vue',
+    solid: './output/solid',
+    svelte: './output/svelte'
+  }
+};
